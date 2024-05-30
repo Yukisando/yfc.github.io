055ab30ed985e7dff1ad50b3176da9790a505092
Nathan <decastronathan@gmail.com>
2024-05-30 11:44:19 +0200

    file split

---
+++ scripts.js
@@ -0,0 +1,42 @@
+async function fetchPosts() {
+    try {
+        const response = await fetch("posts.json");
+        const posts = await response.json();
+        posts.forEach(createPost);
+    } catch (error) {
+        console.error("Error fetching posts:", error);
+    }
+}
+
+function createPost(post) {
+    const postElement = document.createElement("div");
+    postElement.className = "post";
+    let imagesHtml = "";
+    post.images.forEach((img) => {
+        imagesHtml += `<img src="${img}" alt="post image">`;
+    });
+    postElement.innerHTML = `
+        <h3>${post.date}</h3>
+        <div>${post.content}</div>
+        <div>${imagesHtml}</div>
+      `;
+    document.getElementById("posts-container").appendChild(postElement);
+}
+
+function scrollToPosition() {
+    const scrollTop =
+        window.innerHeight + window.scrollY >= document.body.offsetHeight
+            ? 0
+            : document.body.scrollHeight;
+    window.scrollTo({ top: scrollTop, behavior: "smooth" });
+}
+
+window.addEventListener("scroll", function () {
+    const button = document.getElementById("scrollButton");
+    button.innerHTML =
+        window.innerHeight + window.scrollY >= document.body.offsetHeight
+            ? "⬆"
+            : "⬇";
+});
+
+fetchPosts();
